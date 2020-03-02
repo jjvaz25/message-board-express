e56cf390d89f0f84d27c51c2ddb2216c336ace40
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-03-02 15:59:16 -0500

    loop through messages and render view on home page

--- message-board/routes/index.js
+++ message-board/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  { text: 'Hi there. First post!', user: 'Jeffrey', posted: new Date() },
+  { text: 'Second post!', user: 'Sally', posted: new Date() },
+  { text: 'Third post!', user: 'Bianca', posted: new Date() },
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Mini Messageboard',
+    messages,
+    title2: 'jeff'
+  });
 });
+
+
+
 
 module.exports = router;
